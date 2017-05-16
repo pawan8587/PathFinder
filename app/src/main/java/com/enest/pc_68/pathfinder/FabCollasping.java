ceff209ed23eb4aeee4ffc1f24565813ba55e8b3
pawan8587 <pawanbishnoi1458@gmail.com>
2017-05-16 13:50:07 +0530

    Bottom sheet Added

--- app/src/main/java/com/enest/pc_68/pathfinder/FabCollasping.java
+++ app/src/main/java/com/enest/pc_68/pathfinder/FabCollasping.java
@@ -1,15 +1,11 @@
 package com.enest.pc_68.pathfinder;
 
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
-import com.enest.pc_68.pathfinder.coordinatorCustomLibs.BottomSheetBehaviorGoogleMapsLike;
-import com.enest.pc_68.pathfinder.coordinatorCustomLibs.MergedAppBarLayoutBehavior;
 
 public class FabCollasping extends AppCompatActivity {
 
@@ -25,22 +21,6 @@ public class FabCollasping extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setTitle("Home");
         }
-
-
-        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
-        View bottomSheet = coordinatorLayout.findViewById(R.id.bottom_sheet);
-        final BottomSheetBehaviorGoogleMapsLike behavior = BottomSheetBehaviorGoogleMapsLike.from(bottomSheet);
-
-      /*   AppBarLayout mergedAppBarLayout = (AppBarLayout) findViewById(R.id.merged_appbarlayout);
-       MergedAppBarLayoutBehavior mergedAppBarLayoutBehavior = MergedAppBarLayoutBehavior.from(mergedAppBarLayout);
-        mergedAppBarLayoutBehavior.setToolbarTitle("Messages");
-        mergedAppBarLayoutBehavior.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                behavior.setState(BottomSheetBehaviorGoogleMapsLike.STATE_COLLAPSED);
-            }
-        });
-        behavior.setState(BottomSheetBehaviorGoogleMapsLike.STATE_COLLAPSED);*/
 
     }
 }
